1577cf2c4dc86352a87267166de831b281bdff6d
xander <1149277941@qq.com>
2024-07-20 19:10:58 +0800

    feat: 支持自动抽卡

---
+++ action.py
@@ -0,0 +1,130 @@
+from enum import Enum
+from typing import Tuple
+import pyautogui
+
+
+class Pixel:
+    def __init__(self, px: Tuple[int, ...]):
+        self.px = px
+
+    @staticmethod
+    def in_deviation(a: int, b: int, deviation: int) -> bool:
+        if a > b:
+            return b + deviation > a
+        return a + deviation >= b
+
+    def like(self, px: 'Pixel', deviation=6) -> bool:
+        if len(self.px) != len(px.px):
+            return False
+        for i in range(len(px.px)):
+            if not self.in_deviation(self.px[i], px.px[i], deviation):
+                return False
+        return True
+
+
+def get_color(x: int, y: int) -> Pixel:
+    im = pyautogui.screenshot()
+    return Pixel(im.getpixel((x, y)))
+
+
+class Page:
+    def __str__(self) -> str:
+        money = ''
+        if self.need_gold:
+            money += 'gold'
+        if self.need_diamond:
+            money += 'diamond'
+        return (
+            f'money={money} white={self.white_card} blue={self.blue_card} '
+            f'purple={self.purple_card} yellow={self.yellow_card} red={self.red_card}')
+
+    def __init__(self):
+        self.need_diamond = False
+        self.need_gold = False
+        self.white_card = 0
+        self.blue_card = 0
+        self.purple_card = 0
+        self.yellow_card = 0
+        self.red_card = 0
+
+        self.im = pyautogui.screenshot()
+
+        self.update_state()
+
+        self.update_card_num(106, 556)
+        self.update_card_num(360, 622)
+        self.update_card_num(622, 556)
+        self.total_card_num = self.white_card + self.blue_card + self.purple_card + self.yellow_card + self.red_card
+
+    def update_state(self):
+        px = Pixel(self.im.getpixel((388, 1124)))
+        if px.like(Pixel((146, 212, 81, 255))):
+            self.need_diamond = True
+
+        px = Pixel(self.im.getpixel((378, 1126)))
+        if px.like(Pixel((247, 201, 78, 255))):
+            self.need_gold = True
+
+    def update_card_num(self, x: int, y: int):
+        px = Pixel(self.im.getpixel((x, y)))
+        if px.like(Pixel((169, 165, 158, 255))):
+            self.white_card += 1
+        if px.like(Pixel((72, 138, 206, 255))):
+            self.blue_card += 1
+        if px.like(Pixel((168, 63, 242, 255))):
+            self.purple_card += 1
+        if px.like(Pixel((245, 194, 70, 255))):
+            self.yellow_card += 1
+        if px.like(Pixel((200, 54, 61, 255))):
+            self.red_card += 1
+
+    def is_unknown_status(self):
+        return (not self.need_diamond and not self.need_gold) or self.total_card_num != 3
+
+    def need_confirm(self) -> bool:
+        px1 = Pixel(self.im.getpixel((354, 980)))
+        px2 = Pixel(self.im.getpixel((620, 980)))
+        px3 = Pixel(self.im.getpixel((410, 1030)))
+        return (px1.like(Pixel((218, 88, 78, 255))) and
+                px2.like(Pixel((97, 198, 97, 255))) and
+                px3.like(Pixel((218, 223, 230, 255))))
+
+
+class Action:
+    # 招募
+    @staticmethod
+    def enlist():
+        # 像素点位置不等同于鼠标坐标系位置
+        pyautogui.moveTo(218, 564)
+        pyautogui.click()
+
+    # 加注
+    @staticmethod
+    def increase_bet():
+        pyautogui.moveTo(322, 519)
+        pyautogui.click()
+
+    # 放弃
+    @staticmethod
+    def give_up():
+        pyautogui.moveTo(211, 613)
+        pyautogui.click()
+
+    # 确认放弃招募剩下的高品质角色
+    @staticmethod
+    def confirm_give_up():
+        pyautogui.moveTo(277, 484)
+        pyautogui.click()
+
+    # 鼠标离开游戏窗口
+    @staticmethod
+    def mouse_leaved() -> bool:
+        px = pyautogui.position()
+        return px.x > 410 or px.y > 756
+
+    # 鼠标复位，避免干扰像素判定
+    @staticmethod
+    def mouse_reset(click=False):
+        pyautogui.moveTo(207, 45)
+        if click:
+            pyautogui.click()
---
+++ app.py
@@ -0,0 +1,54 @@
+import time
+
+import pyautogui
+
+import action
+
+
+def log(msg: str, wait: float = 3):
+    ts = time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(time.time()))
+    print(ts, msg)
+    if wait > 0:
+        time.sleep(wait)
+
+
+def run(times=100):
+    action.Action.mouse_reset(True)
+
+    for i in range(times):
+        if action.Action.mouse_leaved():
+            log("mouse has leaved the window, exit", 0)
+            return
+        action.Action.mouse_reset()
+        page = action.Page()
+        print(page)
+        if page.is_unknown_status():
+            if page.need_confirm():
+                action.Action.confirm_give_up()
+                log("confirm give up")
+                continue
+            log("unknown status")
+            continue
+        if page.purple_card == 3 or page.yellow_card > 0 or page.red_card > 0:
+            action.Action.increase_bet()
+            log("increase bet", 1)
+        if page.need_gold:
+            action.Action.enlist()
+            log("enlist success")
+            continue
+        if page.red_card > 1:
+            log("amazing event, wait for you", 0)
+            return
+        action.Action.give_up()
+        log("give up")
+
+
+def show():
+    page = action.Page()
+    page.im.save('screenshot.png')
+    print(page)
+
+
+if __name__ == '__main__':
+    run()
+    # show()
